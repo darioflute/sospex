91565f499a5261a119e5d59e1529dbc64f56887e
Dario Fadda <darioflute@gmail.com>
2017-03-31 14:46:41 -0700

    version 0.5 setup,meta

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='sospex',
-      version='0.4.alpha',
+      version='0.5.alpha',
       description='SOFIA SPectrum EXplorer',
       long_description='The package displays FIFI-LS cubes',
       author='Dario Fadda',
