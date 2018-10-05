6fc3652f5848a674f35609c841d0937f2261a631
Dario Fadda <darioflute@gmail.com>
2018-10-05 14:49:35 -0700

    for next distribution 0.34.beta

--- setup.py
+++ setup.py
@@ -7,20 +7,32 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 config = {
-      'name':'sospex',
-      'version':'0.33.beta',
-      'description':'SOFIA SPectral EXplorer',
-      'long_description':'The package displays FIFI-LS and GREAT cubes',
-      'author':'Dario Fadda',
-      'author_email':'darioflute@gmail.com',
-      'url':'https://github.com/darioflute/sospex.git',
-      'download_url':'https://github.com/darioflute/sospex',
-      'license':'GPLv3+',
-      'packages':['sospex'],
-      'scripts':['bin/sospex'],
-      'include_package_data':True,
-      'package_data':{'sospex':['copyright.txt','icons/*.png','icons/*.gif',
-                                'help/*.html','yellow-stylesheet.css']}
+        'name':'sospex',
+        'version':'0.34.beta',
+        'description':'SOFIA SPectral EXplorer',
+        'long_description':long_description,
+        'long_description_type':"text/markdown",
+        'author':'Dario Fadda',
+        'author_email':'darioflute@gmail.com',
+        'url':'https://github.com/darioflute/sospex.git',
+        'download_url':'https://github.com/darioflute/sospex',
+        'license':'GPLv3+',
+        'packages':['sospex'],
+        'scripts':['bin/sospex'],
+        'install_requires':[
+                'astropy>=3.0',
+                'lmfit',
+                'reproject',
+                'matplotlib>=2.1,<3.0'
+                ],
+        'include_package_data':True,
+        'package_data':{'sospex':['copyright.txt','icons/*.png','icons/*.gif',
+                                  'help/*.html','yellow-stylesheet.css']},
+        'classifiers':[
+                "Programming Language :: Python :: 3",
+                "License :: OSI Approved :: GPLv3+ License",
+                "Operating System :: OS Independent",
+                ],
      }
 
 setup(**config)
