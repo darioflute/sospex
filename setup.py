6e13e8dfdc2d704f1ee002bb373169ddb84ee9d9
Dario Fadda <darioflute@gmail.com>
2018-10-05 14:23:43 -0700

    modified setup

--- setup.py
+++ setup.py
@@ -1,18 +1,26 @@
 #!/usr/bin/env python
+"""Setup script to install sospex."""
 
-from distutils.core import setup
+from setuptools import setup
 
-setup(name='sospex',
-      version='0.33.beta',
-      description='SOFIA SPectrum EXplorer',
-      long_description='The package displays FIFI-LS cubes',
-      author='Dario Fadda',
-      author_email='darioflute@gmail.com',
-      url='https://github.com/darioflute/sospex.git',
-      download_url='https://github.com/darioflute/sospex',
-      license='GPLv3+',
-      packages=['sospex'],
-      scripts=['bin/sospex'],
-      include_package_data=True,
-      package_data={'sospex':['copyright.txt','icons/*.png','icons/*.gif','help/*.html','yellow-stylesheet.css']}
-     )
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+config = {
+      'name':'sospex',
+      'version':'0.33.beta',
+      'description':'SOFIA SPectral EXplorer',
+      'long_description':'The package displays FIFI-LS and GREAT cubes',
+      'author':'Dario Fadda',
+      'author_email':'darioflute@gmail.com',
+      'url':'https://github.com/darioflute/sospex.git',
+      'download_url':'https://github.com/darioflute/sospex',
+      'license':'GPLv3+',
+      'packages':['sospex'],
+      'scripts':['bin/sospex'],
+      'include_package_data':True,
+      'package_data':{'sospex':['copyright.txt','icons/*.png','icons/*.gif',
+                                'help/*.html','yellow-stylesheet.css']}
+     }
+
+setup(**config)
